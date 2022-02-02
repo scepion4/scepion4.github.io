a90b7a0d67081dc038cd14c7068260c862af01ae
scepion4 <safonov04@ukr.net>
2022-02-02 18:17:09 +0200

    добавил картинки в карусель

--- src/components/cars.jsx
+++ src/components/cars.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import tauran from '../img/tauran.jpg'
-import t5 from '../img/t5.jpeg'
-import fabia from '../img/fabia.jpg'
+import vw from '../img/wv t5.jpg'
+import fabia from '../img/skoda2.jpg'
+import octavia from '../img/skoda.jpg'
+import mercedes from '../img/mercedes.jpg'
+import opel1 from '../img/opel.jpg'
+import opel2 from '../img/opel2.jpg'
+import outlander from '../img/outlander.jpg'
+
+
 export default function Cars() {
   return (
   <div style={{display:'flex',justifyContent:'center'}}>
@@ -13,7 +20,6 @@ export default function Cars() {
        className="d-block w-100"
        src={tauran}
        alt="First slide"
-       style={{objectFit:'cover'}}
      />
      <Carousel.Caption>
        <h3>VW tauran</h3>
@@ -22,9 +28,8 @@ export default function Cars() {
    <Carousel.Item>
      <img
        className="d-block w-100"
-       src={t5}
+       src={vw}
        alt="Second slide"
-       style={{objectFit:'cover'}}
      />
  
      <Carousel.Caption>
@@ -36,14 +41,68 @@ export default function Cars() {
        className="d-block w-100"
        src={fabia}
        alt="Third slide"
-       style={{objectFit:'cover'}}
      />
  
      <Carousel.Caption>
-       <h3>SKODA</h3>
+       <h3>SKODA fabia</h3>
      </Carousel.Caption>
    </Carousel.Item>
- </Carousel>
+ <Carousel.Item>
+     <img
+       className="d-block w-100"
+       src={octavia}
+       alt="Third slide"
+     />
+ 
+     <Carousel.Caption>
+       <h3>SKODA octavia</h3>
+     </Carousel.Caption>
+   </Carousel.Item>
+   <Carousel.Item>
+     <img
+       className="d-block w-100"
+       src={mercedes}
+       alt="Third slide"
+     />
+ 
+     <Carousel.Caption>
+       <h3> MERCEDES vito</h3>
+     </Carousel.Caption>
+   </Carousel.Item>
+   <Carousel.Item>
+     <img
+       className="d-block w-100"
+       src={opel1}
+       alt="Third slide"
+     />
+ 
+     <Carousel.Caption>
+       <h3>OPEL vivaro</h3>
+     </Carousel.Caption>
+   </Carousel.Item>
+   <Carousel.Item>
+     <img
+       className="d-block w-100"
+       src={opel2}
+       alt="Third slide"
+     />
+ 
+     <Carousel.Caption>
+       <h3>OPEL vivaro</h3>
+     </Carousel.Caption>
+   </Carousel.Item>
+   <Carousel.Item>
+     <img
+       className="d-block w-100"
+       src={outlander}
+       alt="Third slide"
+     />
+ 
+     <Carousel.Caption>
+       <h3>MITSUBISHI outlander</h3>
+     </Carousel.Caption>
+   </Carousel.Item>
+   </Carousel>
  </div>
   )
 }
